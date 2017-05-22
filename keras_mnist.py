e3b5298664119b56ae7895bdc42738b9d4076c0f
Rahav lussato <RahavLussato@gmail.com>
2017-05-22 13:13:02 +0300

    update eras example

--- keras_mnist.py
+++ keras_mnist.py
@@ -6,26 +6,27 @@ Gets to 99.25% test accuracy after 12 epochs
 16 seconds per epoch on a GRID K520 GPU.
 '''
 from __future__ import print_function
-import numpy as np
-
-import missinglink
-
-np.random.seed(1337)  # for reproducibility
 import argparse
 import os
-
 import pwd
 
+import numpy as np
+import logging
+
+import missinglink
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+
+np.random.seed(1337)  # for reproducibility
 
 
 def get_username():
     return pwd.getpwuid(os.getuid())[0]
+
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
@@ -36,6 +37,7 @@ parser.add_argument('--host')
 
 args = parser.parse_args()
 
+logging.getLogger('missinglink').setLevel(logging.DEBUG)
 batch_size = 128
 nb_classes = 10
 
@@ -72,39 +74,47 @@ print(X_test.shape[0], 'test samples')
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-model = Sequential()
 
-model.add(Convolution2D(
-    nb_filters, kernel_size[0], kernel_size[1],
-    input_shape=input_shape))
+def create_model():
+    model = Sequential()
 
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
+    model.add(Conv2D(
+        nb_filters, (kernel_size[0], kernel_size[1]),
+        input_shape=input_shape))
 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.25))
 
-model.compile(
-    loss='categorical_crossentropy',
-    optimizer='adadelta',
-    metrics=['accuracy', 'categorical_accuracy', 'mean_squared_error', 'hinge', 'precision', 'fbeta_score'])
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adadelta',
+        metrics=['accuracy', 'categorical_accuracy', 'mean_squared_error', 'hinge'])
+
+    return model
+
+
+temp_model = create_model()
+
 
 callback = missinglink.KerasCallback(owner_id=args.owner_id, project_token=args.project_token, host=args.host)
 
 callback.set_properties(display_name='KerasMinstTest', description='cool kerassing around')
 
-model.fit(
-    X_train, Y_train, batch_size=batch_size, nb_epoch=args.epochs, validation_split=0.2,
+temp_model.fit(
+    X_train, Y_train, batch_size=batch_size, epochs=args.epochs, validation_split=0.2,
     callbacks=[callback])
 
-score = model.evaluate(X_test, Y_test, verbose=0)
+score = temp_model.evaluate(X_test, Y_test, verbose=0)
 
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
