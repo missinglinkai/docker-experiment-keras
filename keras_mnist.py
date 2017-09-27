4586a55bcd101033c4476b821857112d5eff7f52
Rahav lussato <rahav@yallo.com>
2017-09-27 12:46:23 +0300

    add sampling option

--- keras_mnist.py
+++ keras_mnist.py
@@ -17,12 +17,14 @@ import argparse
 import os
 import pwd
 
+
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+from random import randint
 
 
 def get_username():
@@ -34,6 +36,7 @@ parser.add_argument('--owner-id', required=True)
 parser.add_argument('--project-token', required=True)
 parser.add_argument('--epochs', type=int, default=8)
 parser.add_argument('--batch-size', type=int, default=128)
+parser.add_argument('--is-sampling', type=bool, default=False)
 parser.add_argument('--host')
 
 args = parser.parse_args()
@@ -52,6 +55,15 @@ kernel_size = (3, 3)
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+if args.is_sampling:
+    random_sampling_factor = randint(0, 50)
+    random_sampling_factor = random_sampling_factor / float(100)
+
+    rows_to_delete = range(int(random_sampling_factor * X_train.shape[0]))
+
+    X_train = np.delete(X_train, rows_to_delete, 0)
+    y_train = np.delete(y_train, rows_to_delete, 0)
 
 if K.image_dim_ordering() == 'th':
     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
@@ -101,6 +113,9 @@ model.compile(
 callback = missinglink.KerasCallback(owner_id=args.owner_id, project_token=args.project_token, host=args.host)
 
 callback.set_properties(display_name='KerasMinstTest', description='cool kerassing around')
+
+if args.is_sampling:
+    callback.set_hyperparams(sampling_factor=random_sampling_factor)
 
 model.fit(
     X_train, Y_train, batch_size=batch_size, nb_epoch=args.epochs, validation_split=0.2,
